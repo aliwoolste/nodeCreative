95b1f40e6b8fcf2025cfba0469aa166357c70b5c
Alison Woolstenhulme <aliwoolste@msn.com>
2017-11-02 19:09:08 -0600

    inital commit

---
+++ public/javascripts/app.js
@@ -0,0 +1,32 @@
+angular.module('app', [])
+  .controller('mainCtrl', mainCtrl)
+ .directive('chat', chatDirective);
+
+function mainCtrl ($scope) {
+  
+$scope.users = [];
+  $scope.addNew = function (user) {
+	$scope.users.push({ 
+      name: user.name,
+      message: user.message,
+  });
+user.name = '';
+    user.message = ''; 
+};
+}
+function chatDirective () {
+  return {
+    scope: {
+      user: '=' 
+    },
+    restrict: 'E', 
+    replace: 'true',
+    template: (
+      '<div class="Chat1">' +
+          '<h4>{{user.name}}</h4>' +
+	  '<h4>{{user.message}}</h4>' +
+      '</div>'
+    ), 
+    link: link
+  };
+}
---
+++ routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var messages = [
+  {
+    name: 'Liz',
+    message: 'Hello'
+  },
+  {
+    name: 'Trevor',
+    message: 'How\'s it going?'
+  }
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/', function(req, res) {
+  res.sendFile('index.html', {root: 'public'});
+});
+
+router.get('/chat', function(req,res) {
+  console.log("In chat");
+  res.send(messages);
+});
+
+router.post('/chat', function(req, res) {
+  console.log("In Chat Post");
+  console.log(req.body);
+  messages.push(req.body);
+  res.end('{"success" : "Updated Successfully", "status" : 200}');
+});
+
+module.exports = router;
