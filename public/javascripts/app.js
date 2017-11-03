7f3e1cabcf52e225ad682c11a3116e38633d4df5
hlysm377 <alyssa.m.howard94@gmail.com>
2017-11-03 13:00:53 -0600

    Updating app.js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,54 @@
-angular.module('app', [])
+var app = window.angular.module('app', [])
+
+app.factory('messageFetcher', messageFetcher)
+app.controller('mainCtrl', mainCtrl)
+
+function messageFetcher ($http) {
+
+  var API_ROOT = 'message'
+  return {
+    get: function () {
+      return $http
+        .get(API_ROOT)
+        .then(function (resp) {
+          return resp.data
+        })
+    }
+  }
+
+}
+
+function mainCtrl ($scope, messageFetcher, $http) {
+
+  $scope.message = []
+
+  messageFetcher.get()
+    .then(function (data) {
+      $scope.message = data
+    })
+
+  $scope.addMes = function() {
+    var formData = {name:$scope.Name,mes:$scope.Mes};
+    console.log(formData);
+    var mesURL = 'message';
+    $http({
+       url: mesURL,
+       method: "POST",
+       data: formData
+    }).success(function(data, status, headers, config) {
+      console.log("Post worked");
+    }).error(function(data, status, headers, config) {
+      console.log("Post failed");
+    });
+    function refresh() {
+      window.location.reload();
+    }
+    refresh();
+  }
+}
+
+
+/*angular.module('app', [])
 .controller('mainCtrl', mainCtrl)
 .directive('chat', chatDirective);
 
@@ -38,4 +88,4 @@ return {
   ), 
 };
 }
-
+*/
