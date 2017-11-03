8e2bb555805fa577c49835948d5cf3e83e7a4c7d
hlysm377 <alyssa.m.howard94@gmail.com>
2017-11-03 13:03:24 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,33 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-var messages = [
-  {
+var chat = [
+  /*{
     name: 'Liz',
     message: 'Hello'
   },
   {
     name: 'Trevor',
     message: 'How\'s it going?'
-  }
+  }*/
 ];
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
+router.get('/', function(req, res, next) {
+   res.render('index', { title: 'Express' });
+ });
 
 router.get('/', function(req, res) {
   res.sendFile('node.html', {root: 'public'});
 });
 
-router.get('/chat', function(req,res) {
-  console.log("In chat");
-  res.send(messages);
+router.get('/message', function(req,res) {
+  console.log("In Messages");
+  res.send(chat);
 });
 
-router.post('/chat', function(req, res) {
-  console.log("In Chat Post");
+router.post('/message', function(req, res) {
+  console.log("In Messages Post");
   console.log(req.body);
   messages.push(req.body);
   res.end('{"success" : "Updated Successfully", "status" : 200}');
